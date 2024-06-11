c79f8dde478af75ecfda72b70ca2c337a9ba0b7b
msain1995 <h.z.nasab@gmail.com>
2024-06-11 16:09:46 +0100

    Model-DS Pickles are created!

--- FTProject02/src/data/make_dataset.py
+++ FTProject02/src/data/make_dataset.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import shutil
+import numpy as np
+import pickle
 
 # Hourly Prediction  (Use Dataset as it is)
 # Daily Prediction   (Reorder Based on Date)
@@ -17,53 +19,61 @@ def Train_Test_Split_OnYear(
     data = pd.read_csv(path_to_file)
     train_set = data[data[key_to_split] < value_to_split]
     test_set  = data[data[key_to_split] >= value_to_split]
+    # print(f'Trainset-Length: {len(train_set)} Testset-Length:{len(test_set)}')
     return train_set, test_set
 
 def Apply_Scaling(
-        path_to_file: str, 
-        features_to_scale: list, 
+        train_set: pd.DataFrame, 
+        test_set: pd.DataFrame, 
+        scalable_features: list, 
         scaler_type: str = 'standard', 
     ) -> tuple:
-    
-    # Load the dataset
-    df = pd.read_csv(path_to_file)
-    Original_order = df.columns.tolist()
-    
+
+    Original_order = train_set.columns.tolist()
     # Separate features to scale and other columns
-    features = df[features_to_scale]
-    other_columns = df.drop(columns=features_to_scale)
-    
+    train_set_scalable    = train_set[scalable_features]
+    test_set_scalable     = test_set[scalable_features]
+    train_set_categorical = train_set.drop(columns=scalable_features)
+    test_set_categorical  = test_set.drop(columns=scalable_features)
     # Apply the scaler to the features
     scaler = StandardScaler() if scaler_type == 'standard' else MinMaxScaler()
-    scaled_features = scaler.fit_transform(features)
-    
+    train_set_scaled = scaler.fit_transform(train_set_scalable)
+    test_set_scaled  = scaler.transform(test_set_scalable)
     # Convert the scaled features back to a DataFrame
-    scaled_features_df = pd.DataFrame(scaled_features, columns=features_to_scale)
-    
+    train_set_scaled_features = pd.DataFrame(train_set_scaled, columns=scalable_features)
+    test_set_scaled_features  = pd.DataFrame(test_set_scaled,  columns=scalable_features)
     # Combine the scaled features with other columns
-    scaled_data = pd.concat([other_columns, scaled_features_df], axis=1)
-    df_reordered = scaled_data[Original_order]
+    train_set_scaled_data = pd.concat([train_set_categorical.reset_index(drop=True), train_set_scaled_features.reset_index(drop=True)], axis=1)
+    test_set_scaled_data = pd.concat([test_set_categorical.reset_index(drop=True),   test_set_scaled_features.reset_index(drop=True)], axis=1)
 
-    return df_reordered, scaler
+    # Reordering Dataframe as Before
+    train_set_reordered = train_set_scaled_data[Original_order]
+    test_set_reordered = test_set_scaled_data[Original_order]
+
+    return train_set_reordered, test_set_reordered, scaler
 
 def Construct_Hourly_CSV(
         path_to_file:str,
         ctr_code:str,
         cols_to_drp:list = ['Date', 'DayOfYear', 'WeekOfYear', 'Quarter'],
-        ff_format:str='%.5f'
+        ff_format:str='%.5f',
+        include_ID:bool=False
     ) -> None:
     df = pd.read_csv(path_to_file)
     df.drop(columns=cols_to_drp, inplace=True)
 
-    df['ID'] = range(len(df))
-    df = df[['ID'] + [col for col in df.columns if col != 'ID']]
+    if include_ID:
+        df['ID'] = [f'ID-{i}' for i in range(len(df))] 
+        df = df[['ID'] + [col for col in df.columns if col != 'ID']]
+
     df.to_csv(os.path.join(os.path.dirname(path_to_file), f'{ctr_code}_Hourly.csv'), index=False, float_format=ff_format)
 
 def Construct_Daily_CSV(
         path_to_file:str,
         ctr_code:str,
         cols_to_drp:list = ['Date', 'DayOfYear', 'WeekOfYear', 'Quarter'],
-        ff_format:str='%.5f'
+        ff_format:str='%.5f',
+        include_ID:bool=False
     ) -> None:
     df = pd.read_csv(path_to_file)
     df.drop(columns=cols_to_drp, inplace=True)
@@ -78,50 +88,54 @@ def Construct_Daily_CSV(
     'Irrad_difuse': 'mean',
     'Load': 'mean'}).reset_index()
 
-    aggregated_data['ID'] = range(len(aggregated_data))
-    aggregated_data = aggregated_data[['ID'] + [col for col in aggregated_data.columns if col != 'ID']]
+    if include_ID:
+        aggregated_data['ID'] = [f'ID-{i}' for i in range(len(aggregated_data))]
+        aggregated_data = aggregated_data[['ID'] + [col for col in aggregated_data.columns if col != 'ID']]
+        
     aggregated_data.to_csv(os.path.join(os.path.dirname(path_to_file), f'{ctr_code}_Daily.csv'), index=False, float_format=ff_format)
 
 def Construct_Weekly_CSV(
         path_to_file:str,
         ctr_code:str,
         cols_to_drp:list = ['Date', 'DayOfYear', 'Quarter', 'WeekOfYear'],
-        ff_format:str='%.5f'
+        ff_format:str='%.5f',
+        include_ID:bool=False
     ) -> None:
     df = pd.read_csv(path_to_file)
     df.drop(columns=cols_to_drp, inplace=True)
 
     aggregated_data = df.groupby(['Year', 'Month', 'WeekOfMonth']).agg({
-    'IsWeekend': 'first',
-    'IsHoliday': 'first',
     'Temperature': 'mean',
     'Irrad_direct': 'mean',
     'Irrad_difuse': 'mean',
     'Load': 'mean'}).reset_index()
 
-    aggregated_data['ID'] = range(len(aggregated_data))
-    aggregated_data = aggregated_data[['ID'] + [col for col in aggregated_data.columns if col != 'ID']]
+    if include_ID:
+        aggregated_data['ID'] = [f'ID-{i}' for i in range(len(aggregated_data))]
+        aggregated_data = aggregated_data[['ID'] + [col for col in aggregated_data.columns if col != 'ID']]
+
     aggregated_data.to_csv(os.path.join(os.path.dirname(path_to_file), f'{ctr_code}_Weekly.csv'), index=False, float_format=ff_format)
 
 def Construct_Monthly_CSV(
         path_to_file:str,
         ctr_code:str,
         cols_to_drp:list = ['Date', 'DayOfYear', 'WeekOfYear', 'Quarter'],
-        ff_format:str='%.5f'
+        ff_format:str='%.5f',
+        include_ID:bool=False
     ) -> None:
     df = pd.read_csv(path_to_file)
     df.drop(columns=cols_to_drp, inplace=True)
 
     aggregated_data = df.groupby(['Year', 'Month']).agg({
-    'IsWeekend': 'first',
-    'IsHoliday': 'first',
     'Temperature': 'mean',
     'Irrad_direct': 'mean',
     'Irrad_difuse': 'mean',
     'Load': 'mean'}).reset_index()
 
-    aggregated_data['ID'] = range(len(aggregated_data))
-    aggregated_data = aggregated_data[['ID'] + [col for col in aggregated_data.columns if col != 'ID']]
+    if include_ID:
+        aggregated_data['ID'] = [f'ID-{i}' for i in range(len(aggregated_data))]
+        aggregated_data = aggregated_data[['ID'] + [col for col in aggregated_data.columns if col != 'ID']]
+
     aggregated_data.to_csv(os.path.join(os.path.dirname(path_to_file), f'{ctr_code}_Monthly.csv'), index=False, float_format=ff_format)
 
 def Copy_CSVs_For_Dataset(
@@ -146,30 +160,87 @@ def Manage_CSVs(
         Construct_Monthly_CSV(os.path.join(path_to_file, f'{str_code}_base.csv'), str_code, ff_format=ff_format)
         os.remove(os.path.join(path_to_file, f'{str_code}_base.csv'))
 
-def Create_Hourly_Dataset() -> None:
-    pass
+def Create_Sequence(
+        dataset: pd.DataFrame, 
+        look_back:int,
+        target_column:int = -1     
+    ) -> tuple:
 
-def Create_Daily_Dataset() -> None:
-    pass
+    nrows, ncols = dataset.shape
+    X = []
+    Y = []
+    
+    for i in range(look_back, nrows):
+        X.append(dataset.iloc[i - look_back: i, :].values) # dataset.columns != target_column
+        Y.append(dataset.iloc[i, :]) # dataset.columns != target_column
+    
+    return np.array(X), np.array(Y)
 
-def Create_Monthly_Dataset() -> None:
-    pass
+def Create_Dataset(
+        path_to_file:str,
+        key_to_split:str,
+        value_to_split:int,
+        features_to_scale:list,
+        look_back:int
+    ):
+    train_set, test_set = Train_Test_Split_OnYear(path_to_file, key_to_split, value_to_split)
+    train_set_scaled, test_set_scaled, scaler = Apply_Scaling(train_set, test_set, features_to_scale)
+    
+    train_set_X, train_set_Y = Create_Sequence(train_set_scaled, look_back)
+    test_set_X, test_set_Y   = Create_Sequence(test_set_scaled, look_back)
 
-def Create_Weekly_Dataset() -> None:
-    pass
+    return train_set_X, train_set_Y, test_set_X, test_set_Y, scaler
 
-def Manage_Datasets() -> None:
-    pass
+def Manage_Datasets(
+        path_to_dir:str,
+        key_to_split:str,
+        value_to_split:int,
+        features_to_scale:list,
+        look_back:int   
+    ) -> dict:
+    saved_dict = {}
+    filenames = os.listdir(path_to_dir)
+    contains_name = lambda s, names: any(name in s for name in names)
+    for fname in filenames:
+        if contains_name(fname, ['Daily', 'Hourly', 'Weekly', 'Monthly']):
+            train_set_X, train_set_Y, \
+            test_set_X, test_set_Y,   \
+            scaler = Create_Dataset(os.path.join(path_to_dir, fname),
+                                    key_to_split,
+                                    value_to_split,
+                                    features_to_scale,
+                                    look_back)
+            
+            saved_dict[os.path.splitext(fname)[0]] = {
+                'trX': train_set_X,
+                'trY': train_set_Y,
+                'tsX': test_set_X,
+                'tsY': test_set_Y,
+                'scaler': scaler
+            }
+        
+    with open(os.path.join(path_processed, 'Processed-Dataset.pickle'), 'wb') as f:
+        pickle.dump(saved_dict, f)
+    
+    return saved_dict
 
 if __name__ == "__main__":
+    np.set_printoptions(formatter={'float': lambda x: "{:5.1f}".format(x)})
     # Important Paths
     path_processed = "../../data/processed"
     path_interim = "../../data/interim"
     ctr_code = ['ES', 'PT', 'PL', 'FR', 'SE']
     features_to_scale = ['Temperature', 'Irrad_direct', 'Irrad_difuse', 'Load']
     ff_format = '%.4f'
+    key_to_split = 'Year'
+    value_to_split = 2019
+    look_back=2
+    
     Copy_CSVs_For_Dataset(path_interim, path_processed)
     Manage_CSVs(path_processed, ctr_code, ff_format)
+    mydict = Manage_Datasets(path_processed, key_to_split, value_to_split, features_to_scale, look_back)
 
-
-
+    # print(f'PT_Monthly:: Trainset-Length: {len(mydict["PT_Monthly"]["trX"])} Testset-Length:{len(mydict["PT_Monthly"]["tsX"])}')
+    # print(mydict["PT_Monthly"]["trX"])
+    # print(' ')    
+    # print(mydict["PT_Monthly"]["trY"])    
